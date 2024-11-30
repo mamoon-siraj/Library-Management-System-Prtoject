5625a8ab2042e83db732312348603fd438767757
AnasImran-146 <anas.imran290907@gmail.com>
2024-11-30 14:51:36 +0500

    merged all the functions . librarian part is now running succesfully

--- libraryManagementSystem.cpp
+++ libraryManagementSystem.cpp
@@ -24,7 +24,7 @@ int main(){
         break;
 
         case 2:
-            student_menu();   
+            //student_menu();   
         break;
 
         default:
@@ -60,6 +60,37 @@ void librarian_menu(){
             cout<<"2.View Books."<<endl;
         break;
     }
+}
+
+void add_books(){
+
+    int serial_no;
+    string book_title;
+    string author_name;
+
+    ofstream file("Books.txt", ios::app); 
+
+    if (file.is_open()) { 
+        cout<<"Enter Serial No."<<endl;
+        cin>>serial_no;
+
+        cin.ignore();
+
+        cout<<"Enter book title"<<endl;
+        getline(cin, book_title);
+
+        cout<<"Enter Author name."<<endl;
+        getline(cin, author_name);
+
+        file<<serial_no<<"\t"<<book_title<<" by "<<author_name<<endl;
+    }
+    else{
+        cout<<"Error. Could not open the file."<<endl;
+    }
+
+    file.close();
+    cout << "Text added to the file successfully."<<endl;
+    
 }
 
 void display_books(){
